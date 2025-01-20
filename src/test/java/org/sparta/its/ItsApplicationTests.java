4dd561d8116c740854f61b6af5feae24b47bab35
kimuky <xogus559@gmail.com>
2025-01-20 19:40:54 +0900

    test

--- src/test/java/org/sparta/its/ItsApplicationTests.java
+++ src/test/java/org/sparta/its/ItsApplicationTests.java
@@ -3,7 +3,7 @@ package org.sparta.its;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-// @SpringBootTest
+@SpringBootTest
 class ItsApplicationTests {
 
 	@Test
