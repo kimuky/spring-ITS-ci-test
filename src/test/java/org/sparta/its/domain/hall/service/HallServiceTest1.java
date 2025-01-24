c624c1e41ea0b1df2994301739fa3bedb7235bb4
kimuky <xogus559@gmail.com>
2025-01-24 11:14:25 +0900

    test

--- src/test/java/org/sparta/its/domain/hall/service/HallServiceTest1.java
+++ src/test/java/org/sparta/its/domain/hall/service/HallServiceTest1.java
@@ -33,7 +33,7 @@ class HallServiceTest1 {
 	// 	userService.signUp(new AuthRequest.SignUpDto("test@test.com", "asd!@#D23", "kim", "010-1111-2222", "USER"));
 	// }
 
-	@DisplayName("3")
+	@DisplayName("4")
 	@Test
 	void 테스트2() {
 		User byIdOrThrow = userRepository.findByIdOrThrow(2L);
