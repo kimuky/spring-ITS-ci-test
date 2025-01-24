1087f4603d12b40e50f9bf223a3fabc2f3df8667
kimuky <xogus559@gmail.com>
2025-01-24 11:56:19 +0900

    test

--- src/test/java/org/sparta/its/domain/hall/service/HallServiceTest1.java
+++ src/test/java/org/sparta/its/domain/hall/service/HallServiceTest1.java
@@ -17,7 +17,7 @@ import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 
 @SpringBootTest
-@Testcontainers
+// @Testcontainers
 @Sql("/db/dumd.sql")
 class HallServiceTest1 {
 
