ec623b92be95af8f6ec25c6e4a5d3a93670e4595
kimuky <xogus559@gmail.com>
2025-02-03 10:43:20 +0900

    test

--- src/test/resources/db/dumd.sql
+++ src/test/resources/db/dumd.sql
@@ -4,3 +4,11 @@ VALUES ('test1@test.com', 'sdfsdf!@#123', 'kim', '010-2222-2222', 'USER', 'ACTIV
 
 INSERT INTO "user" (email, password, name, phone_number, role, status)
 VALUES ('test2@test.com', 'sdfsdf!@#123', 'kim', '010-2222-2222', 'USER', 'ACTIVATED');
+
+
+INSERT INTO "user" (email, password, name, phone_number, role, status)
+VALUES ('test3@test.com', 'sdfsdf!@#123', 'kim', '010-2222-2222', 'USER', 'ACTIVATED');
+
+
+INSERT INTO "user" (email, password, name, phone_number, role, status)
+VALUES ('test4@test.com', 'sdfsdf!@#123', 'kim', '010-2222-2222', 'USER', 'ACTIVATED');
